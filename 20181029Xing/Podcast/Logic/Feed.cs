f66f9c45bc7fa854db1c9261b81c11c6ac10338c
ironxing <liuxing77777@gmail.com>
2018-10-29 22:40:53 +0100

    mån kväll

---
+++ 20181029Xing/Podcast/Logic/Feed.cs
@@ -0,0 +1,58 @@
+﻿using Podcast.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podcast.Logic
+{
+
+
+    public class Feed
+    {
+        public string Titel { get; set; }
+        public int AntalAvsnitt { get; set; }
+        public int Frekvens { get; set; }
+        public string Url { get; set; }
+        public string Kategori { get; set; }
+
+        public List<Avsnitt> List {get; set;}
+
+        public List<Avsnitt> SortedList
+        {
+            get
+            {
+                return List.OrderBy(i => i.Titel).ToList();
+            }
+        }
+
+        public Feed(int Frekvens, string Url, string Kategori)
+        {
+            this.Frekvens = Frekvens;
+            this.Url = Url;
+            this.Kategori = Kategori;
+            this.List = new List<Avsnitt>();
+        }
+
+        public Feed()
+        {
+
+        }
+
+        public void GetFeedInfo()
+        {
+            FeedDB.GetFeedInfo(this);
+        }
+
+        public List<Avsnitt> GetAvsnitts()
+        {
+            return FeedDB.GetAvsnitts(this.Url);
+        }
+
+        public void saveAvsnitt()
+        {
+            AvsnittDB.saveAvsnitt(this.List, this.Titel);
+        }
+    }
+}
